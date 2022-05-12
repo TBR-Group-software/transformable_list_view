bfade8377a3d6240c3b311e8b66c77690aca6b23
theSharpestTool <gavrik.pixel@gmail.com>
2022-05-12 10:54:13 +0300

    example screen created

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:example/screens/example_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -7,16 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'Transformable List View Example',
+      home: ExampleScreen(),
     );
   }
 }
