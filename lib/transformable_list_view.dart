f67cff308eeb4be96f1355bb3b4bc1129f868554
theSharpestTool <gavrik.pixel@gmail.com>
2022-07-13 17:12:27 +0300

    dublicate export removed

--- lib/transformable_list_view.dart
+++ lib/transformable_list_view.dart
@@ -5,4 +5,3 @@ export 'src/transformable_list_item.dart';
 export 'src/transformable_list_view.dart';
 export 'src/transformable_sliver.dart';
 export 'src/transformable_sliver_list.dart';
-export 'src/transformable_list_item.dart';
