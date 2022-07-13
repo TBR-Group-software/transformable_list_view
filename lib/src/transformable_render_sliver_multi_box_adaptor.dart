62e50feab1a2722412bf734c20bd23694c1c1755
theSharpestTool <gavrik.pixel@gmail.com>
2022-07-13 17:07:21 +0300

    TransformableRenderSliverMultiBoxAdaptor added

---
+++ lib/src/transformable_render_sliver_multi_box_adaptor.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/rendering.dart';
+import 'package:transformable_list_view/src/transform_matrix_callback.dart';
+import 'package:transformable_list_view/src/transformable_list_item.dart';
+
+mixin TransformableRenderSliverMultiBoxAdaptor on RenderSliverMultiBoxAdaptor {
+  abstract final TransformMatrixCallback getTransformMatrix;
+
+  /// transform of each child
+  final cachedTransforms = <RenderBox, Matrix4>{};
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    //// Copied from [RenderSliverMultiBoxAdaptor] except the OVERRIDE
+
+    if (firstChild == null) return;
+    // offset is to the top-left corner, regardless of our axis direction.
+    // originOffset gives us the delta from the real origin to the origin in the axis direction.
+    final Offset mainAxisUnit, crossAxisUnit, originOffset;
+    final bool addExtent;
+    switch (applyGrowthDirectionToAxisDirection(
+        constraints.axisDirection, constraints.growthDirection)) {
+      case AxisDirection.up:
+        mainAxisUnit = const Offset(0.0, -1.0);
+        crossAxisUnit = const Offset(1.0, 0.0);
+        originOffset = offset + Offset(0.0, geometry!.paintExtent);
+        addExtent = true;
+        break;
+      case AxisDirection.right:
+        mainAxisUnit = const Offset(1.0, 0.0);
+        crossAxisUnit = const Offset(0.0, 1.0);
+        originOffset = offset;
+        addExtent = false;
+        break;
+      case AxisDirection.down:
+        mainAxisUnit = const Offset(0.0, 1.0);
+        crossAxisUnit = const Offset(1.0, 0.0);
+        originOffset = offset;
+        addExtent = false;
+        break;
+      case AxisDirection.left:
+        mainAxisUnit = const Offset(-1.0, 0.0);
+        crossAxisUnit = const Offset(0.0, 1.0);
+        originOffset = offset + Offset(geometry!.paintExtent, 0.0);
+        addExtent = true;
+        break;
+    }
+    RenderBox? child = firstChild;
+    while (child != null) {
+      final double mainAxisDelta = childMainAxisPosition(child);
+      final double crossAxisDelta = childCrossAxisPosition(child);
+      Offset childOffset = Offset(
+        originOffset.dx +
+            mainAxisUnit.dx * mainAxisDelta +
+            crossAxisUnit.dx * crossAxisDelta,
+        originOffset.dy +
+            mainAxisUnit.dy * mainAxisDelta +
+            crossAxisUnit.dy * crossAxisDelta,
+      );
+      if (addExtent) childOffset += mainAxisUnit * paintExtentOf(child);
+
+      // If the child's visible interval (mainAxisDelta, mainAxisDelta + paintExtentOf(child))
+      // does not intersect the paint extent interval (0, constraints.remainingPaintExtent), it's hidden.
+      if (mainAxisDelta < constraints.remainingPaintExtent &&
+          mainAxisDelta + paintExtentOf(child) > 0) {
+        //// ---------------↓↓↓OVERRIDE↓↓↓---------------
+        final paintTransform = getTransformMatrix(
+          TransformableListItem(
+            offset: childOffset,
+            size: child.size,
+            constraints: constraints,
+            index: child is RenderIndexedSemantics ? child.index : null,
+          ),
+        );
+        cachedTransforms[child] = paintTransform;
+
+        context.pushTransform(
+          needsCompositing,
+          childOffset,
+          paintTransform,
+          (context, offset) => context.paintChild(child!, offset),
+
+          ///TODO add [oldLayer] for perfomance optimization
+        );
+
+        //// ---------------↑↑↑OVERRIDE↑↑↑---------------
+      }
+
+      child = childAfter(child);
+    }
+  }
+}
--- lib/src/transformable_sliver_list.dart
+++ lib/src/transformable_sliver_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'package:transformable_list_view/src/transform_matrix_callback.dart';
 import 'package:transformable_list_view/src/transformable_list_item.dart';
 import 'package:transformable_list_view/src/transformable_render_sliver_helpers.dart';
+import 'package:transformable_list_view/src/transformable_render_sliver_multi_box_adaptor.dart';
 
 /// Extends [SliverList] with [getTransformMatrix] callback that allows to add transform animations.
 class TransformableSliverList extends SliverList {
@@ -28,11 +29,11 @@ class TransformableSliverList extends SliverList {
 }
 
 class TransformableRenderSliverList extends RenderSliverList
-    with TransformableRenderSliverHelpers {
+    with
+        TransformableRenderSliverMultiBoxAdaptor,
+        TransformableRenderSliverHelpers {
+  @override
   final TransformMatrixCallback getTransformMatrix;
-
-  /// transform of each child
-  final _cachedTransforms = <RenderBox, Matrix4>{};
 
   TransformableRenderSliverList({
     required RenderSliverBoxChildManager childManager,
@@ -40,87 +41,6 @@ class TransformableRenderSliverList extends RenderSliverList
   }) : super(childManager: childManager);
 
   @override
-  void paint(PaintingContext context, Offset offset) {
-    //// Copied from [RenderSliverMultiBoxAdaptor] except the OVERRIDE
-
-    if (firstChild == null) return;
-    // offset is to the top-left corner, regardless of our axis direction.
-    // originOffset gives us the delta from the real origin to the origin in the axis direction.
-    final Offset mainAxisUnit, crossAxisUnit, originOffset;
-    final bool addExtent;
-    switch (applyGrowthDirectionToAxisDirection(
-        constraints.axisDirection, constraints.growthDirection)) {
-      case AxisDirection.up:
-        mainAxisUnit = const Offset(0.0, -1.0);
-        crossAxisUnit = const Offset(1.0, 0.0);
-        originOffset = offset + Offset(0.0, geometry!.paintExtent);
-        addExtent = true;
-        break;
-      case AxisDirection.right:
-        mainAxisUnit = const Offset(1.0, 0.0);
-        crossAxisUnit = const Offset(0.0, 1.0);
-        originOffset = offset;
-        addExtent = false;
-        break;
-      case AxisDirection.down:
-        mainAxisUnit = const Offset(0.0, 1.0);
-        crossAxisUnit = const Offset(1.0, 0.0);
-        originOffset = offset;
-        addExtent = false;
-        break;
-      case AxisDirection.left:
-        mainAxisUnit = const Offset(-1.0, 0.0);
-        crossAxisUnit = const Offset(0.0, 1.0);
-        originOffset = offset + Offset(geometry!.paintExtent, 0.0);
-        addExtent = true;
-        break;
-    }
-    RenderBox? child = firstChild;
-    while (child != null) {
-      final double mainAxisDelta = childMainAxisPosition(child);
-      final double crossAxisDelta = childCrossAxisPosition(child);
-      Offset childOffset = Offset(
-        originOffset.dx +
-            mainAxisUnit.dx * mainAxisDelta +
-            crossAxisUnit.dx * crossAxisDelta,
-        originOffset.dy +
-            mainAxisUnit.dy * mainAxisDelta +
-            crossAxisUnit.dy * crossAxisDelta,
-      );
-      if (addExtent) childOffset += mainAxisUnit * paintExtentOf(child);
-
-      // If the child's visible interval (mainAxisDelta, mainAxisDelta + paintExtentOf(child))
-      // does not intersect the paint extent interval (0, constraints.remainingPaintExtent), it's hidden.
-      if (mainAxisDelta < constraints.remainingPaintExtent &&
-          mainAxisDelta + paintExtentOf(child) > 0) {
-        //// ---------------↓↓↓OVERRIDE↓↓↓---------------
-        final paintTransform = getTransformMatrix(
-          TransformableListItem(
-            offset: childOffset,
-            size: child.size,
-            constraints: constraints,
-            index: child is RenderIndexedSemantics ? child.index : null,
-          ),
-        );
-        _cachedTransforms[child] = paintTransform;
-
-        context.pushTransform(
-          needsCompositing,
-          childOffset,
-          paintTransform,
-          (context, offset) => context.paintChild(child!, offset),
-
-          ///TODO add [oldLayer] for perfomance optimization
-        );
-
-        //// ---------------↑↑↑OVERRIDE↑↑↑---------------
-      }
-
-      child = childAfter(child);
-    }
-  }
-
-  @override
   Matrix4 getCurrentTransform(RenderBox child) =>
-      _cachedTransforms[child] ?? Matrix4.identity();
+      cachedTransforms[child] ?? Matrix4.identity();
 }
